04e9ff288f78eaaf215845fba4f382879377283c
Ahren09 <1099052927@qq.com>
2019-03-07 15:21:13 -0800

    Compatible to all ASCII strings

--- Gee-nomics/Trie.h
+++ Gee-nomics/Trie.h
@@ -27,52 +27,52 @@ private:
     
     TrieNode* root;
     
-    int toIndex(const char c) const
-    {
-        switch(c)
-        {
-            case 'A':
-                return 0;
-                break;
-            case 'C':
-                return 1;
-                break;
-            case 'G':
-                return 2;
-                break;
-            case 'T':
-                return 3;
-                break;
-            default:
-                return -1;
-                break;
-        }
-        
-    }
-    
-    char toBase(const int i) const
-    {
-        {
-            switch(i)
-            {
-                case 0:
-                    return 'A';
-                    break;
-                case 1:
-                    return 'C';
-                    break;
-                case 2:
-                    return 'G';
-                    break;
-                case 3:
-                    return 'T';
-                    break;
-                default:
-                    return 'X';
-                    break;
-            }
-        }
-    }
+//    int toIndex(const char c) const
+//    {
+//        switch(c)
+//        {
+//            case 'A':
+//                return 0;
+//                break;
+//            case 'C':
+//                return 1;
+//                break;
+//            case 'G':
+//                return 2;
+//                break;
+//            case 'T':
+//                return 3;
+//                break;
+//            default:
+//                return -1;
+//                break;
+//        }
+//
+//    }
+//
+//    char toBase(const int i) const
+//    {
+//        {
+//            switch(i)
+//            {
+//                case 0:
+//                    return 'A';
+//                    break;
+//                case 1:
+//                    return 'C';
+//                    break;
+//                case 2:
+//                    return 'G';
+//                    break;
+//                case 3:
+//                    return 'T';
+//                    break;
+//                default:
+//                    return 'X';
+//                    break;
+//            }
+//        }
+//    }
     
     std::vector<ValueType> searchNode(TrieNode* p, const std::string& key,int i, bool exactMatchOnly)
     {
@@ -91,22 +91,21 @@ private:
         }
         
         char c=key[i];
-        int index=toIndex(c);
         
         vector<ValueType>* tmp;
-        if(p->children[index])
+        if(p->children[c])
         {
-            tmp=&searchNode(p->children[index], key, i+1, exactMatchOnly);
+            tmp=&searchNode(p->children[c], key, i+1, exactMatchOnly);
             v.insert(v.end(),tmp->begin(),tmp->end());
         }
         
         //If we do NOT require exact match
         if(!exactMatchOnly)
         {
-            for(int j=0;j<4;i++)
+            for(int j=0;j<128;i++)
             {
                 //If children TrieNode is empty
-                if(j==index)
+                if(j==c)
                     continue;
                 tmp=&searchNode(p->children[j],key, i+1, true);
                 v.insert(v.end(),tmp->begin(),tmp->end());
@@ -125,7 +124,7 @@ Trie::Trie()
 Trie::~Trie()
 {
     if(root)
-        for(int i=0;i<4;i++)
+        for(int i=0;i<128;i++)
         {
             delete root->children[i];
         }
@@ -146,18 +145,12 @@ void Trie::insert(const std::string& key, const ValueType& value)
         return;
     
     TrieNode* p=root;
-    int i;
     for(char c:key)
     {
-        i=toBase(c);
         
-        //If key contains bases other than A,C,G,T
-        if(i==-1)
-            return;
-        
-        if(!p->children[i])
-            p->children[i]=new TrieNode();
-        p=p->children[i];
+        if(!p->children[c])
+            p->children[c]=new TrieNode();
+        p=p->children[c];
     }
     p->val.push_back(value);
     
@@ -173,8 +166,9 @@ std::vector<ValueType> find(const std::string& key, bool exactMatchOnly) const
         return v;
     
     TrieNode* p=root;
-    //Now key contains at least one element
-    int index=toIndex(key[0]);
+    
+    //Now key contains at least one element long
+    int index=key[0];
     if(index==-1 || children[index]==nullptr)
         return v;
     else
@@ -190,9 +184,6 @@ std::vector<ValueType> find(const std::string& key, bool exactMatchOnly) const
     
     v=searchNode(p,key,1,exactMatchOnly);
     return v;
-    
-    
-    
 }
 
 
