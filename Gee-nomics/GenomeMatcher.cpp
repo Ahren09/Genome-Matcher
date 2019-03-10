33c410103156b2962b8667e2ccfb79fee9dbadf6
Ahren09 <1099052927@qq.com>
2019-03-09 21:55:44 -0800

    Redo addGenome()

--- Gee-nomics/GenomeMatcher.cpp
+++ Gee-nomics/GenomeMatcher.cpp
@@ -16,7 +16,7 @@ public:
     bool findRelatedGenomes(const Genome& query, int fragmentMatchLength, bool exactMatchOnly, double matchPercentThreshold, vector<GenomeMatch>& results) const;
 private:
     std::vector<const Genome*> genomes;
-    Trie<int> trie;
+    Trie<pair<const Genome*,int>> trie;
     int min_searchLength;
 };
 
@@ -35,11 +35,27 @@ void GenomeMatcherImpl::addGenome(const Genome& genome)
     if(len<min_searchLength)
         return;
     
-    for(int i=0;min_searchLength+i<=len;i++)
+    //Extract genome to be inserted from certain point
+    string fragment;
+    genome.extract(0, len, fragment);
+    
+    //i: starting position of DNA sequence at corresponding genome
+    for(int i=0;i+min_searchLength<=len;i++)
     {
-        string fragment;
-        genome.extract(i, min_searchLength, fragment);
-        trie.insert(fragment, i);
+        
+        //j:length of segment to be inserted with start position at i
+        for(int j=min_searchLength;j+i<=len;j++)
+        {
+            //TODO: what if previously already inserted?
+            //Construct the pair to be inserted into the trie
+            
+            //first: vector of Genome* with correspoinding starting node
+            //second: index of start position
+            pair<const Genome*,int> p;
+            p.first=&genome;
+            p.second=i;
+            trie.insert(fragment.substr(i,j), p);
+        }
     }
 }
 
@@ -50,8 +66,73 @@ int GenomeMatcherImpl::minimumSearchLength() const
 
 bool GenomeMatcherImpl::findGenomesWithThisDNA(const string& fragment, int minimumLength, bool exactMatchOnly, vector<DNAMatch>& matches) const
 {
-    if(minimumLength<min_searchLength)
+    int frag_len=fragment.size();
+    
+    if(minimumLength<min_searchLength || frag_len<minimumLength)
         return false;
+    
+    vector<pair<const Genome*, int>> result;
+    
+    //i is the starting position of fragment to search
+    for(int i=0;i+minimumLength<=frag_len;i++)
+    {
+        //j is the length of currently searching fragment
+        for(int j=min_searchLength; j+i<=frag_len; j++)
+        {
+            result=trie.find(fragment.substr(i,j),exactMatchOnly);
+            
+            //IF NOT found, then no longer sequence with starting point at i can be found
+            if(result.empty())
+            {
+                break;
+            }
+            
+            //traverse through the result
+            for(vector<pair<const Genome*, int>>::iterator p=result.begin();p!=result.end();p++)
+            {
+                const Genome* currentGenome=p->first;
+                string currentGenomeName=currentGenome->name();
+                int currentGenomeStartPosition=p->second;
+                
+                
+                vector<DNAMatch>::iterator matches_it=matches.begin();
+                for(;matches_it!=matches.end();matches_it++ )
+                {
+                    //If DNA sequence already inserted into vector<DNAMatch>& matches
+                    //Meaning that segments with same starting sequence appear >=twice
+                    if(matches_it->genomeName==currentGenomeName)
+                    {
+                        //If current Genome has length longer than previous DNAMatch
+                        if(j>matches_it->length)
+                        {
+                            matches_it->length=j;
+                            matches_it->position=currentGenomeStartPosition;
+                        }
+                        
+                    }
+                    
+                }
+                
+                //If DNA sequence NOT inserted into vector<DNAMatch>& matches
+                if(matches_it==matches.end())
+                {
+                    DNAMatch d;
+                    d.genomeName=currentGenomeName;
+                    d.position=currentGenomeStartPosition;
+                    d.length=j;
+                }
+                
+                
+                
+                
+            }
+            
+            
+            
+        }
+    }
+    
+    
 }
 
 bool GenomeMatcherImpl::findRelatedGenomes(const Genome& query, int fragmentMatchLength, bool exactMatchOnly, double matchPercentThreshold, vector<GenomeMatch>& results) const
